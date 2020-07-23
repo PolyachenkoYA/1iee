373f261f1668dfdb13d4b55a99c6b74d58d6f52f
Yura <polyachenkoya@gmail.com>
2020-07-23 15:49:29 +0300

    update before RASA cluster

--- src/exe/chim2solv.sh
+++
@@ -1,8 +0,0 @@
-set -e
-cd ../run
-
-gmx pdb2gmx -f 1iee_wions_cr.pdb -o 1iee_wions_cr.gro -water spce
-gmx editconf -f 1iee_wions_cr.gro -o 1iee_wions_cr_cent.gro -c
-gmx solvate -cp 1iee_wions_cr_cent.gro -cs spc216.gro -o 1iee_solv.gro -p topol.top
-
-cd ../init
